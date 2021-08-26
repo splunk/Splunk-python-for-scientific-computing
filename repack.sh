7898a6014341b53415eee77b0364a8dac271027e
pinarkavak <pkavak@splunk.com>
2021-08-26 14:26:16 -0700

    MLA-3395: Fix CI for linux and windows

--- repack.sh
+++ repack.sh
@@ -270,7 +270,15 @@ elif [[ $MODE -eq 4 ]]; then
             echo "Builds are available at:"
             echo "${ARTIFACTORY_URL}/${TARGET_FOLDER}"
         else
-            echo "[ERROR] Publish only master branch and tags, tag needs to match build script version too"
+            if [[ -z "$CI_MERGE_REQUEST_IID" || "$CI_MERGE_REQUEST_IID" == " " ]]; then
+              echo "Merge Request ID is empty : $CI_MERGE_REQUEST_IID"
+              echo "[ERROR] Publish only master branch, merge_requests and tags, tag needs to match build script version too"
+            else
+              TARGET_FOLDER="${ARTIFACTORY_TARGET}/builds/merge_requests/MR${CI_MERGE_REQUEST_IID}"
+              jfrog rt u "build/*.tgz" "${TARGET_FOLDER}/" --sync-deletes="${TARGET_FOLDER}/Splunk_SA_Scientific_Python_*.tgz" --quiet --fail-no-op
+              echo "Builds are available at:"
+              echo "${ARTIFACTORY_URL}/${TARGET_FOLDER}"
+            fi
         fi
     fi
 fi
