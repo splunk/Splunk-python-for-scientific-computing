d9dfa85be70e381bff3d298ae0155f8210242a2f
Pinar Kavak <pkavak@splunk.com>
2024-07-18 06:44:31 +0000

    MLA-4739 psc windows updates es

--- scripts/ps/miniconda_settings.ps1
+++ scripts/ps/miniconda_settings.ps1
@@ -1,4 +1,4 @@
 # Miniconda information
 # See the latest of miniconda builds from https://repo.anaconda.com/miniconda/
-$script:MINICONDA_VERSION = "py38_23.1.0-1"
-$script:WINDOWS_X86_64_SHA256 = "4178df2a15284fd07b10c5fad592e5c15e6be5bfc37ee90d8e02bbde7792f6f9"
+$script:MINICONDA_VERSION = "py38_23.5.0-3"
+$script:WINDOWS_X86_64_SHA256 = "a643675ca68f7c0577864e20f73615a52aeb9c07663576411a86964326fe4288"
