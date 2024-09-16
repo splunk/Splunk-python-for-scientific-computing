be280a7fbfa87d4c6a95b136455bdaa33ced69f4
uvakalapudi <uvakalapudi@splunk.com>
2024-09-16 23:19:49 +0530

    Fixing fossa command issue for runner

--- scripts/ps/fossa.ps1
+++ scripts/ps/fossa.ps1
@@ -4,9 +4,9 @@ $script:SCRIPT_DIR=$PSScriptRoot
 $env:Path += ";$($MINICONDA_BUILD_DIR);$(Join-Path $MINICONDA_BUILD_DIR "Scripts");$(Join-Path $MINICONDA_BUILD_DIR "Library\bin")"
 
 if (-Not $env:CI) {
-    & "$(Join-Path $env:LOCALAPPDATA "fossa-cli\fossa.exe")" analyze --only-target "conda" --only-path $PLATFORM --debug --team "FOSSA Sandbox" --title "PSC Test"
+    & fossa analyze --only-target "conda" --only-path $PLATFORM --debug --team "FOSSA Sandbox" --title "PSC Test"
 } else {
     $script:PROJECT_TITLE = "Python for Scientific Computing ${PLATFORM}"
     $script:PROJECT_NAME = "${env:CI_PROJECT_URL}/-/tree/${env:CI_COMMIT_REF_NAME}/${PLATFORM}"
-    & "$(Join-Path $env:LOCALAPPDATA "fossa-cli\fossa.exe")" analyze --only-target "conda" --only-path $PLATFORM -p $PROJECT_NAME -b ${env:CI_COMMIT_REF_NAME} --title $PROJECT_TITLE --team "${env:CI_PROJECT_URL}"
+    & fossa analyze --only-target "conda" --only-path $PLATFORM -p $PROJECT_NAME -b ${env:CI_COMMIT_REF_NAME} --title $PROJECT_TITLE --team "${env:CI_PROJECT_URL}"
 }
